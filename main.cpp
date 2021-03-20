17eee04b5a1bc828696661b1b3f28d749d643f5d
Jackie Ocaña <45646092+jackattackks@users.noreply.github.com>
2021-03-20 14:49:59 -0700

    new project created
    had breakpoint error in previous files, so created new one with successful built.

--- main.cpp
+++ main.cpp
@@ -1,19 +1,83 @@
 //
 //  main.cpp
-//  Week9Midterm
+//  Week9Midterm2
 //
-//  Created by Jackie on 3/16/21.
+//  Created by Jackie on 3/20/21.
 //
 
 #include <iostream>
 using namespace std;
 #include <string>
-#include "Whole.hpp"
-#include "Part.hpp"
 
+class Whole {
+private:
+    string wholeName;
+public:
+
+    void testDefault (){
+        wholeName = "None ";
+
+        cout << "In whole default constructor." << endl;
+        cout << "Called default constructor." << endl;
+        cout << "Contents in name: " << wholeName << endl;
+    };
+    
+    void testParm(string name){
+        wholeName = name;
+        cout << wholeName;
+        cout << " in whole parm constructor" << endl;
+        cout << "called parm constructor." << endl;
+        cout << "Contents in name: " << wholeName << endl;
+    }
+    
+    void getName(){
+        cout << wholeName << endl;
+    }
+    
+    ~Whole(){
+        cout << "Called destructor." << endl;
+        cout <<"Contents in variable: " << wholeName << endl;
+    }
+};
+
+class Part {
+    void constructor(){
+        cout << "In default part constructor." << endl;
+ 
+    }
+    
+    ~Part(){
+        //nothing in here, per instructions
+    }
+};
+
+
+//MAIN FUNCTION HERE
 
 int main() {
     cout <<"Name: Jackie Ocaña - Midterm Part 2 - March 16, 2021" << endl;
+    cout << endl;
+    
     Whole W1;
-    W1.default2();
+    W1.testDefault();
+    W1.~Whole();
+    
+
+    cout << endl;
+    
+    Whole W2;
+    W2.testParm("W2");
+    W2.~Whole();
+    
+
+    cout << endl;
+    
+    cout << "Dot notation result: ";
+    W1.getName();
+    cout << "Dot notation result: ";
+    W2.getName();
+    cout << endl;
+    
+    cout << "Destructors get called again." << endl;
+    return 0;
 }
